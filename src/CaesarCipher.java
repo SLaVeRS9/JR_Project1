675ac1e8146d4a9b320eeb92bf75a880a2dba471
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-02-11 03:49:39 +0300

    added class CyrillicAlphabetForCipher and did realization added class Menu

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,4 +1,16 @@
+import AlphabetsForChiper.CyrillicAlphabetForCipher;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class CaesarCipher {
+    CyrillicAlphabetForCipher cyrillicAlphabetForCipher;
+
+    private final static char[] VALID_CHARACTERS = {'А', 'b'};
+    private final static Set<Character> VALID_CHARACTERS1 = new HashSet();
+    private final static ArrayList<Character> VALID_CHARACTERS2 = new ArrayList<>();
+
     public static void encoder(){
 
     }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import AlphabetsForChiper.CyrillicAlphabetForCipher;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        CyrillicAlphabetForCipher cyrillicAlphabetForCipher = CyrillicAlphabetForCipher.getInstance();
+        ArrayList<Integer> integers = cyrillicAlphabetForCipher.getCyrillicAlphabetCodesAndSymbols();
+        System.out.println(integers);
+        int size = integers.size();
+        System.out.println(integers.get(0));
+        System.out.println(integers.get(0)+1);
+        integers.set(0, 10);
+        System.out.println(integers.get(0));
+        /*Character[] chars = {'А', 'Б', 'а', 'б', 122};
+        char[] chars2 = {'А', 'Б', 'а', 'б', 122};
+        char res = 1041;
+        char A = 'А';
+        char a = 'а';
+        int res2 = String.valueOf(a).getBytes()[0];
+        int res3 = a;
+        System.out.println((int)a);
+        System.out.println((int)A);
+        System.out.println(res);*/
 
     }
 }
