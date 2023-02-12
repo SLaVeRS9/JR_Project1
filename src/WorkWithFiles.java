f2ee6635286ec4163bba05a375a41196dfc4fce2
SLaVeRS9-HonorLaptop <slavers9@outlook.com>
2023-02-12 14:04:35 +0300

    done get data from file

---
+++ src/WorkWithFiles.java
@@ -0,0 +1,26 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class WorkWithFiles {
+    
+    //TODO Создать собственное исключение для работы с файлами
+    public static byte[] readDataFromFile(Path path){
+        validatePath(path);
+        byte[] data = Files.readAllBytes(path);
+        return data;
+    }
+
+    //TODO Создать собственное исключение для работы с файлами
+    public static void validatePath(Path path){
+            path.isAbsolute();
+    }
+
+    //TODO Создать собственное исключение для работы с файлами
+    public static void validatePath(String stringPath){
+        Path path = Path.of(stringPath);
+        path.isAbsolute();
+}
+
+
+
+}
