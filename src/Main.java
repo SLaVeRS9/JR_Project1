f6691d816b10932948796be47b402d0bf46644d0
SLaVeRS9 <SLaVeRS9@outlook.com>
2023-02-17 02:14:03 +0300

    delete inactive packages

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import AlphabetsForCipher.Alphabet;
-import AlphabetsForCipher.CyrillicAlphabet;
-import AlphabetsForCipher.MainSymbols;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
